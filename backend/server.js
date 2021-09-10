b67a42948451b44c31bb5e18b6935b70bd5bc266
shivam7374 <shivamsingla491@gmail.com>
2021-09-10 11:46:32 +0530

    DF

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,11 @@ import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 import orderRouter from './routers/orderRouter.js';
 import uploadRouter from './routers/uploadRouter.js';
+import dialogflow from '@google-cloud/dialogflow';
+import {v4 as uuidv4} from 'uuid';
+// console.log("Hello this is console")
+// console.log(uuidv4());
+
 
 dotenv.config();
 
@@ -32,11 +37,11 @@ app.use('/api/orders', orderRouter);
 app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
-const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-app.use(express.static(path.join(__dirname, '/frontend/build')));
+const __dir = path.resolve();
+app.use('/uploads', express.static(path.join(__dir, '/uploads')));
+app.use(express.static(path.join(__dir, '/frontend/build')));
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+  res.sendFile(path.join(__dir, '/frontend/build/index.html'))
 );
 
 app.use((err, req, res, next) => {
@@ -47,3 +52,53 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
 });
+
+
+// const dialogflow = require('@google-cloud/dialogflow');
+// const uuid = require('uuid');
+
+/**
+ * Send a query to the dialogflow agent, and return the query result.
+ * @param {string} projectId The project to be used
+ */
+async function runSample(projectId = 'conersational-chat-naiv') {
+  // A unique identifier for the given session
+  const sessionId = uuidv4();
+
+  // Create a new session
+  const sessionClient = new dialogflow.SessionsClient({
+      keyFilename:"D:/Github Projects/Thunderbolt/backend/conersational-chat-naiv-507565148141.json"
+  });
+  const sessionPath = sessionClient.projectAgentSessionPath(
+    projectId,
+    sessionId
+  );
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        // The query to send to the dialogflow agent
+        text: 'i want to learn dialogflow',
+        // The language used by the client (en-US)
+        languageCode: 'en-US',
+      },
+    },
+  };
+
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  console.log('Detected intent');
+  const result = responses[0].queryResult;
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
+  if (result.intent) {
+    console.log(`  Intent: ${result.intent.displayName}`);
+  } else {
+    console.log('  No intent matched.');
+  }
+}
+
+runSample();
+
