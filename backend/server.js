67199fb18f7e3978c8bacb62e7f1a054eb413a6c
shivam7374 <shivamsingla491@gmail.com>
2021-09-10 13:32:50 +0530

    Add

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,6 @@ import userRouter from './routers/userRouter.js';
 import orderRouter from './routers/orderRouter.js';
 import uploadRouter from './routers/uploadRouter.js';
 
-// import dialogflow from '@google-cloud/dialogflow';
-// import {v4 as uuidv4} from 'uuid';
-// console.log("Hello this is console")
-// console.log(uuidv4());
 
 dotenv.config();
 
@@ -52,3 +48,56 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
 });
+
+
+
+// import dialogflow from '@google-cloud/dialogflow';
+// import {v4 as uuidv4} from 'uuid';
+// console.log("Hello this is console")
+// console.log(uuidv4());
+
+// /**
+//  * Send a query to the dialogflow agent, and return the query result.
+//  * @param {string} projectId The project to be used
+//  */
+//  async function runSample(projectId = 'conersational-chat-naiv') {
+//   // A unique identifier for the given session
+//   const sessionId = uuidv4();
+
+//   // Create a new session
+//   const sessionClient = new dialogflow.SessionsClient({
+//       keyFilename:path.join(__dirname, '/backend/conersational-chat-naiv-507565148141.json')
+//   });
+//   const sessionPath = sessionClient.projectAgentSessionPath(
+//     projectId,
+//     sessionId
+//   );
+
+//   // The text query request.
+//   const request = {
+//     session: sessionPath,
+//     queryInput: {
+//       text: {
+//         // The query to send to the dialogflow agent
+//         text: 'i want to learn dialogflow',
+//         // The language used by the client (en-US)
+//         languageCode: 'en-US',
+//       },
+//     },
+//   };
+
+//   // Send request and log result
+//   const responses = await sessionClient.detectIntent(request);
+//   console.log('Detected intent');
+//   const result = responses[0].queryResult;
+//   console.log(`  Query: ${result.queryText}`);
+//   console.log(`  Response: ${result.fulfillmentText}`);
+//   if (result.intent) {
+//     console.log(`  Intent: ${result.intent.displayName}`);
+//   } else {
+//     console.log('  No intent matched.');
+//   }
+// }
+
+// runSample();
+
