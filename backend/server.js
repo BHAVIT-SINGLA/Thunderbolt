2220d7fceceecc69268ea8a382f9d6b91a71c2c5
shivam7374 <shivamsingla491@gmail.com>
2021-09-10 12:14:18 +0530

    Add

--- backend/server.js
+++ backend/server.js
@@ -57,48 +57,48 @@ app.listen(port, () => {
 // const dialogflow = require('@google-cloud/dialogflow');
 // const uuid = require('uuid');
 
-/**
- * Send a query to the dialogflow agent, and return the query result.
- * @param {string} projectId The project to be used
- */
-async function runSample(projectId = 'conersational-chat-naiv') {
-  // A unique identifier for the given session
-  const sessionId = uuidv4();
+// /**
+//  * Send a query to the dialogflow agent, and return the query result.
+//  * @param {string} projectId The project to be used
+//  */
+// async function runSample(projectId = 'conersational-chat-naiv') {
+//   // A unique identifier for the given session
+//   const sessionId = uuidv4();
 
-  // Create a new session
-  const sessionClient = new dialogflow.SessionsClient({
-      keyFilename:path.join(__dir, '/backend/conersational-chat-naiv-507565148141.json')
-  });
-  const sessionPath = sessionClient.projectAgentSessionPath(
-    projectId,
-    sessionId
-  );
+//   // Create a new session
+//   const sessionClient = new dialogflow.SessionsClient({
+//       keyFilename:path.join(__dir, '/backend/conersational-chat-naiv-507565148141.json')
+//   });
+//   const sessionPath = sessionClient.projectAgentSessionPath(
+//     projectId,
+//     sessionId
+//   );
 
-  // The text query request.
-  const request = {
-    session: sessionPath,
-    queryInput: {
-      text: {
-        // The query to send to the dialogflow agent
-        text: 'i want to learn dialogflow',
-        // The language used by the client (en-US)
-        languageCode: 'en-US',
-      },
-    },
-  };
+//   // The text query request.
+//   const request = {
+//     session: sessionPath,
+//     queryInput: {
+//       text: {
+//         // The query to send to the dialogflow agent
+//         text: 'i want to learn dialogflow',
+//         // The language used by the client (en-US)
+//         languageCode: 'en-US',
+//       },
+//     },
+//   };
 
-  // Send request and log result
-  const responses = await sessionClient.detectIntent(request);
-  console.log('Detected intent');
-  const result = responses[0].queryResult;
-  console.log(`  Query: ${result.queryText}`);
-  console.log(`  Response: ${result.fulfillmentText}`);
-  if (result.intent) {
-    console.log(`  Intent: ${result.intent.displayName}`);
-  } else {
-    console.log('  No intent matched.');
-  }
-}
+//   // Send request and log result
+//   const responses = await sessionClient.detectIntent(request);
+//   console.log('Detected intent');
+//   const result = responses[0].queryResult;
+//   console.log(`  Query: ${result.queryText}`);
+//   console.log(`  Response: ${result.fulfillmentText}`);
+//   if (result.intent) {
+//     console.log(`  Intent: ${result.intent.displayName}`);
+//   } else {
+//     console.log('  No intent matched.');
+//   }
+// }
 
-runSample();
+// runSample();
 
